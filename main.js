f743d4a70d039cd68b668e18f4d5888372de831c
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-03-05 18:10:08 +0200

    Manipulating the DOM and some CSS.

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ const quiz = [
     { name: 'Batman', realName: 'Bruce Wayne' },
 ];
 
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    render (target, content, attributes) {
+        Object.keys(attributes || []).forEach(key => target.setAttribute(key, attributes[ key ]));
+        target.innerHTML = content;
+    }
+};
 
 const game = {
     start (quiz) {
@@ -19,20 +29,24 @@ const game = {
     },
     ask () {
         const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question, question);
         const response = prompt(question);
         this.check(response);
     },
     check (response) {
-        const answer = this.question.realName;
+        const answer = this.question.realName.toLowerCase();
         if (response === answer) {
+            view.render(view.result, 'Correct!', { 'class': 'correct' });
             alert('Correct!');
             this.score++;
+            view.render(view.score, this.score);
         } else {
+            view.render(view.result, `Wrong! The correct answer was ${answer}`, { 'class': 'wrong' });
             alert(`Wrong! The correct answer was ${answer}`);
         }
     },
     gameOver () {
-        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     }
 };
 
