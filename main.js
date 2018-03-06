f9a92865a669d8bd9fa5522f6cf14c89586db6d3
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-03-06 17:20:50 +0200

    No longer uses alerts. Instead implemented a form where the user can answer the questions.

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const view = {
     result: document.getElementById('result'),
     info: document.getElementById('info'),
     start: document.getElementById('start'),
+    response: document.querySelector('#response'),
     render (target, content, attributes) {
         Object.keys(attributes || []).forEach(key => target.setAttribute(key, attributes[ key ]));
         target.innerHTML = content;
@@ -20,6 +21,25 @@ const view = {
     hide (element) {
         element.style.display = 'none';
     },
+    setup () {
+        this.show(this.question);
+        this.show(this.response);
+        this.show(this.result);
+        this.hide(this.start);
+        this.render(this.score, game.score);
+        this.render(this.result, '');
+        this.render(this.info, '');
+        this.resetForm();
+    },
+    resetForm () {
+        this.response.answer.value = '';
+        this.response.answer.focus();
+    },
+    teardown(){
+        this.hide(this.question);
+        this.hide(this.response);
+        this.show(this.start);
+    }
 };
 
 view.start.addEventListener('click', () => game.start(quiz), false);
@@ -27,16 +47,10 @@ view.start.addEventListener('click', () => game.start(quiz), false);
 const game = {
     start (quiz) {
         view.hide(view.start);
-
-        this.questions = [ ...quiz ];
         this.score = 0;
-        // main game loop
-        for (const question of this.questions) {
-            this.question = question;
-            this.ask();
-        }
-        // end of main game loop
-        this.gameOver();
+        this.questions = [ ...quiz ];
+        view.setup();
+        this.ask();
     },
     ask () {
         const question = `What is ${this.question.name}'s real name?`;
@@ -44,20 +58,35 @@ const game = {
         const response = prompt(question);
         this.check(response);
     },
-    check (response) {
+    check (event) {
+        event.preventDefault();
+        const response = view.response.answer.value;
         const answer = this.question.realName.toLowerCase();
         if (response.toLowerCase() === answer) {
             view.render(view.result, 'Correct!', { 'class': 'correct' });
-            alert('Correct!');
             this.score++;
             view.render(view.score, this.score);
         } else {
             view.render(view.result, `Wrong! The correct answer was ${answer}`, { 'class': 'wrong' });
-            alert(`Wrong! The correct answer was ${answer}`);
         }
+        view.resetForm();
+        this.ask();
     },
     gameOver () {
         view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
-        view.show(view.start);
+        view.teardown();
+    },
+    ask (name) {
+        if (this.questions.length > 0) {
+            this.question = this.questions.shift();
+            const question = `What is ${this.question.name}'s real name?`;
+            view.render(view.question, question);
+        }
+        else {
+            this.gameOver();
+        }
     }
 };
+
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
